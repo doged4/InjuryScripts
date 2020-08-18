0595c645339cf0693f3ba681f3e87b5a09d93555
Roland Bainton <rolandbainton@anes-11yj1gq-sw.ucsf.edu>
2020-08-18 12:17:53 -0700

    Adds both means to output, with gene symbols and can deal with Na rows (from excel normally)

--- NewFDR.R
+++ NewFDR.R
@@ -4,7 +4,11 @@ require("stats")
 #cfprank.table <- read.xlsx("/Users/rolandbainton/Desktop/MANCH/02_DEEP_READS_Mnach/Low platelet RANK/WithMockControl_02_DEEP_READS_Manch_CPM_novalues_cutbyCFPrank_pltRanklow.xlsx")
 #platelet.table <- read.xlsx("/Users/rolandbainton/Desktop/MANCH/02_DEEP_READS_Mnach/High platelet RANK/02_DEEP_READS_Manch_CPM_novalues_cutbyCFPrank_pltHigh_aveSD>2.xlsx")
 
-main.data <- main.data
+#initialize getteing data and fixing colnames
+main.data <- main_data
+main.data <- data.frame(main.data)
+
+convertGenes <- read.csv("Downloads/CONVERSIONTABLE_GENE STABLE_ENS_GENENAME.csv")
 
 
 expList <- colnames(main.data)[substr(colnames(main.data),1,2) == "E_"]
@@ -18,8 +22,12 @@ selected.data <- main.data[,c(
   conList
 )]
 
+#get rid of bad rows by gene name
 selected.data <- selected.data[substr(selected.data$Geneid, 1,4) == "ENSG",]
 selected.data[3:length(selected.data)] <- lapply(FUN =  strtoi,X =  selected.data[3:length(selected.data)])
+selected.data <- selected.data[!is.na(selected.data[3]),]
+
+
 
 sumData <- colSums(selected.data[3:length(selected.data)])
 
@@ -42,7 +50,6 @@ selected.data$ci_lower <- NA # lower bound of CI
 selected.data$ci_upper <- NA # upper bound of CI
 
 
-
 for (i in 1:nrow(selected.data)) {
   experimentals = as.numeric(selected.data[i,
                                            expList])
@@ -54,12 +61,20 @@ for (i in 1:nrow(selected.data)) {
   selected.data$mean_diff[i] <- test$estimate[1] - test$estimate[2] #changed to subtract means!!
   selected.data$ci_lower [i] <- test$conf.int[1]
   selected.data$ci_upper [i] <- test$conf.int[2]
+  selected.data$e_mean   [i] <- test$estimate[1]
+  selected.data$c_mean   [i] <- test$estimate[2]
+  
   rm(test)
 }
 selected.data$p_adjusted <- p.adjust(selected.data$p, method = "fdr")
 
 
 out.data <- rbind(c("sums", NA, sumData, NA, NA, NA, NA, NA, NA), selected.data)
+
+
+
+out.data$GenesSymbol <- convertGenes$Gene.name[match(x= out.data$Geneid,table = convertGenes$Gene.stable.ID)]
+
 
 write.table(out.data,
             "~/Desktop/cfpcutoff.mockFdrtest.tsv",
