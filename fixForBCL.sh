1d724dafd38f1b8ea82a2139f58cff5fdbd8dc62
Cevi Bainton <31522721+doged4@users.noreply.github.com>
2020-10-31 20:45:26 -0700

    Fixed to comment out some git stuff

--- fixForBCL.sh
+++ fixForBCL.sh
@@ -3,12 +3,12 @@
 # sed "s:[[:space:]],:,:g" *.csv
 # dos2unix *.csv
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 sed -E "s:[^a-zA-Z0-9,\]],:,:g" $1 > $1.fixed
 dos2unix -n $1.fixed $1
 rm $1.fixed
-=======
+#=======
 #sed "s:[[:space:]],:,:g" $1
-sed "s:[[:space:]],:,:g" $1 | sponge > $1
-dos2unix $1
->>>>>>> 3fd4a145bd073c9c11af013676769d0f2809293d
+#sed "s:[[:space:]],:,:g" $1 | sponge > $1
+#dos2unix $1
+#>>>>>>> 3fd4a145bd073c9c11af013676769d0f2809293d
