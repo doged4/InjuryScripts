b0afc0689ec38c8bd4c74fe6a69f48eefc99eb77
Cevi Bainton <pickledog18@gmail.com>
2022-07-05 11:17:40 -0700

    fixed samtools work with removal, changed names

--- chim_paramtest_STAR_GrCh38.sh
+++ chim_paramtest_STAR_GrCh38.sh
@@ -10,11 +10,13 @@ thisdir=$(pwd);
 #for i in $(ls *R2_001.fastq.gz | rev | cut -c 17- | rev | uniq);  do\
 filename="./samples.txt"
 n=1
-/STAR/bin/Linux_x86_64/STAR --runMode alignReads \
-	--genomeLoad LoadAndExit \
-	--runThreadN 50  \
-	--genomeDir /genome/genomes/Ensembl_Grch38/STAR.Ensembl.GRCh38 \
-	--sjdbGTFfile /genome/genomes/Ensembl_Grch38/Homo_sapiens.GRCh38.100.gtf
+
+#Not working because sjdb setting? seems that cannot do on the fly junction insertion 
+#/STAR/bin/Linux_x86_64/STAR --runMode alignReads \
+#	--genomeLoad LoadAndExit \
+#	--runThreadN 50  \
+#	--genomeDir /genome/genomes/Ensembl_Grch38/STAR.Ensembl.GRCh38 \
+#	--sjdbGTFfile /genome/genomes/Ensembl_Grch38/Homo_sapiens.GRCh38.100.gtf
 
 while read line
 do
@@ -57,12 +59,27 @@ do
 		--chimJunctionOverhangMin 15 \
 		--genomeDir /genome/genomes/Ensembl_Grch38/STAR.Ensembl.GRCh38   --readFilesIn  ${R1_path}   ${R2_path}   --outFileNamePrefix $thisdir/chimeric_STAR.Ensembl.Out/${ID}/${ID}_chimeric_.  --sjdbGTFfile /genome/genomes/Ensembl_Grch38/Homo_sapiens.GRCh38.100.gtf --outSAMtype BAM Unsorted --readFilesCommand zcat;
 
-       samtools sort -@32 -m 4G -o ${ID}_sorted_chimeric.bam ${ID}_chimeric_.Aligned.out.bam 
-       samtools index ${ID}_sorted_chimeric.bam
+#	samtools sort -@32 -m 4G -o ${ID}_sorted_chimeric.bam ${ID}_chimeric_.Aligned.out.bam 
+#	samtools index ${ID}_sorted_chimeric.bam
+#
+#	samtools view -S -b ${ID}_chimeric_.Chimeric.out.sam > ${ID}_chimeric_.Chimeric.out.bam
+#	samtools sort -@32 -m 4G -o ${ID}_sorted_chimeric_.Chimeric.out.bam ${ID}_chimeric_.Chimeric.out.bam
+#	samtools index ${ID}_chimeric_.Chimeric.out.bam
 
-       samtools view -S -b ${ID}_chimeric_.Chimeric.out.sam > ${ID}_chimeric_.Chimeric.out.bam
-       samtools sort -@32 -m 4G -o ${ID}_chimeric_.Chimeric.out.bam ${ID}_chimeric_.Chimeric.aligned.out.bam
-       samtools index ${ID}_chimeric_.Chimeric.aligned.out.bam
+
+	echo "Sorting unambigous bam"
+	samtools sort -@32 -m 4G -o ${ID}_sorted.bam ${ID}_chimeric_.Aligned.out.bam     && rm ${ID}_chimeric_.Aligned.out.bam
+	echo "Indexing unambiguous bam"
+	samtools index ${ID}_sorted.bam
+
+	echo "Converting chimeric sam to bam"
+	samtools view -S -b ${ID}_chimeric_.Chimeric.out.sam > ${ID}.Chimeric.out.bam     && rm ${ID}_chimeric_.Chimeric.out.sam
+	echo "Sorting chimeric bam"
+	samtools sort -@32 -m 4G -o ${ID}.Chimeric_sorted.out.bam ${ID}.Chimeric.out.bam   && rm  ${ID}.Chimeric.out.bam
+	echo "Indexing chimeric bam"
+	samtools index ${ID}.Chimeric_sorted.out.bam
+
+
 	cd ..; cd ..;
 done < $filename
 
@@ -70,11 +87,7 @@ done < $filename
 
 
 
-/STAR/bin/Linux_x86_64/STAR --runMode alignReads \                                
-	        --genomeLoad Remove \                                                
-	        --runThreadN 50  \                                                        
-	        --genomeDir /genome/genomes/Ensembl_Grch38/STAR.Ensembl.GRCh38 \
-        	--sjdbGTFfile /genome/genomes/Ensembl_Grch38/Homo_sapiens.GRCh38.100.gtf  
+# /STAR/bin/Linux_x86_64/STAR --runMode alignReads --genomeLoad Remove --runThreadN 50 --genomeDir /genome/genomes/Ensembl_Grch38/STAR.Ensembl.GRCh38 --sjdbGTFfile /genome/genomes/Ensembl_Grch38/Homo_sapiens.GRCh38.100.gtf  
 
 
 
@@ -88,11 +101,9 @@ cd  ./chimeric_STAR.Ensembl.Out
 #		CHANGES TO MAKE:
 #
 #
-#		delete unsorted bam after sorting
 #		If possible- As soon as alignment is finished,
 #		start sorting AND aligning the next sample.
 #
-#		ALSO- add 'feature count from bam' at end
 
 
 
